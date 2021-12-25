d790dee7559ffe7d3eb9790fb5d9928db21e4351
Daniel Nagy <danielnagy@posteo.de>
2021-12-25 23:58:55 +0100

    Allow more systems for flake

--- flake.nix
+++ flake.nix
@@ -2,31 +2,32 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nagynur.url = "github:nagy/nur-packages";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, nagynur }: rec {
-    defaultPackage.x86_64-linux =
-      nixpkgs.legacyPackages.x86_64-linux.stdenvNoCC.mkDerivation rec {
-        name = "json2dbus";
-        src = self;
-        buildInputs = [
-          (nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (
-            ps: with ps;[
-              nagynur.packages.x86_64-linux.dbussy
-            ]
-          ))
-        ];
-        buildPhase = ''
-          patchShebangs json2dbus
-        '';
-        installPhase = ''
-          install -Dm755 json2dbus $out/bin/json2dbus
-        '';
-        meta = {
-          description = "A tool to translate JSON lines to DBUS messages";
-          license = nixpkgs.lib.licenses.agpl3;
-          platforms = nixpkgs.lib.platforms.linux;
-          homepage = "https://github.com/nagy/json2dbus";
-        };
-      };
-  };
+  outputs = { self, nixpkgs, nagynur, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in with pkgs; rec {
+        defaultPackage =
+          stdenvNoCC.mkDerivation rec {
+            name = "json2dbus";
+            src = self;
+            buildInputs = [
+              (python3.withPackages
+                (ps: with ps; [ nagynur.packages.${system}.dbussy ]))
+            ];
+            buildPhase = ''
+              patchShebangs json2dbus
+            '';
+            installPhase = ''
+              install -Dm755 json2dbus $out/bin/json2dbus
+            '';
+            meta = {
+              description = "A tool to translate JSON lines to DBUS messages";
+              license = nixpkgs.lib.licenses.agpl3;
+              platforms = nixpkgs.lib.platforms.linux;
+              homepage = "https://github.com/nagy/json2dbus";
+            };
+          };
+      });
 }
