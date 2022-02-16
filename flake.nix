f9afa7bbb2bc6b6fba06cda40dffc7883e7813a1
Daniel Nagy <danielnagy@posteo.de>
2022-02-16 20:17:19 +0100

    Cleanup flake

--- flake.nix
+++ flake.nix
@@ -1,33 +1,24 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nagynur.url = "github:nagy/nur-packages";
-    nagynur.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
-  };
-  outputs = { self, nixpkgs, nagynur, flake-utils }:
+  outputs = { self, nixpkgs, nur, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in with pkgs; rec {
-        defaultPackage = stdenvNoCC.mkDerivation rec {
+      with import nixpkgs {
+        inherit system;
+        overlays = [ nur.overlay ];
+      }; {
+        defaultPackage = stdenvNoCC.mkDerivation {
           name = "json2dbus";
           src = self;
           buildInputs = [
-            (python3.withPackages (ps:
-              with ps;
-              [ nagynur.legacyPackages.${system}.python3Packages.dbussy ]))
+            (python3.withPackages
+              (ps: [ pkgs.nur.repos.nagy.python3Packages.dbussy ]))
           ];
-          buildPhase = ''
-            patchShebangs json2dbus
-          '';
           installPhase = ''
             install -Dm755 json2dbus $out/bin/json2dbus
           '';
-          meta = {
+          meta = with lib; {
             description = "A tool to translate JSON lines to DBUS messages";
-            license = nixpkgs.lib.licenses.agpl3;
-            platforms = nixpkgs.lib.platforms.linux;
+            license = licenses.agpl3;
+            platforms = platforms.linux;
             homepage = "https://github.com/nagy/json2dbus";
           };
         };
